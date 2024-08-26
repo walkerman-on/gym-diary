5c99315f65c66d2fa7414c660e3168db533ac1f9
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2024-08-26 22:03:11 +0300

    Deleting fake data

--- src/pages/add-exercise/ui/AddExercisePage.tsx
+++ src/pages/add-exercise/ui/AddExercisePage.tsx
@@ -1,21 +1,20 @@
-import { getDate, getTraining } from 'app/providers/router';
+import { getDate } from 'app/providers/router';
 import { ExerciseSearchForm } from 'widgets/exercise-form/exercise-search-form';
 import { Footer } from "widgets/footer";
-import { ExercisesCard } from "widgets/exercises-card/exercises-card";
 import { Layout } from "pages/layout";
-import { useAuth } from "features/auth/hooks/useAuth";
-import { Navigate, Outlet } from 'react-router-dom';
-import { getLogin } from 'app/providers/router';
-import { CategoriesCard } from 'widgets/categories-card';
+import { Outlet } from 'react-router-dom';
 import React from 'react';
+import { useAppSelector } from 'shared/lib/hooks';
 
 
 export const AddExercisePage = React.memo(() => {
+    const { date, exercises } = useAppSelector(state => state.workout.workout__current)
+
     return (
         <Layout>
             <ExerciseSearchForm />
             <Outlet />
-            <Footer link={getDate()} text="Добавить в тренировку" />
+            <Footer link={getDate(date)} text="Добавить в тренировку" />
         </Layout>
     );
 });
--- src/pages/training/ui/TrainingPage.tsx
+++ src/pages/training/ui/TrainingPage.tsx
@@ -7,44 +7,15 @@ import { Calendar } from 'widgets/calendar';
 import { useAuth } from 'features/auth/hooks/useAuth';
 import { ExercisesWorkoutCard } from 'widgets/exercises-card/exercises-workout-card';
 import { Footer } from 'widgets/footer';
-import { Button } from '@mui/material';
-import { addWorkout, IWorkout } from 'features/workout';
-import { useAppDispatch } from 'shared/lib/hooks';
-import { createWorkout } from 'features/workout/api/createWorkout';
-
-const exerciseInWorkout = {
-  exercise: {
-    categoryId: "FxFF9cC1",
-    id: "ididi",
-    name: "Штанга на рdsdcdcуки",
-    selected: false
-  },
-  sets:
-    [
-      {
-        id: 1,
-        reps: 10,
-        weight: 30
-      }
-    ]
-}
 
 export const TrainingPage = () => {
   const { theme } = useTheme();
   const { isAuth } = useAuth();
-  const { date } = useParams()
 
-  const dispatch = useAppDispatch()
-
-  const addMoreBtnHandler = () => {
-    // dispatch(createWorkout({ date: date }))
-    // dispatch(addWorkout({ date: date, exercise: exerciseInWorkout }))
-  }
   return isAuth ? (
     <main className={classNames('app', cl.TrainingPage, {}, [theme])}>
       <Calendar />
       <ExercisesWorkoutCard />
-      <Button onClick={addMoreBtnHandler}>Добавь упражнение</Button>
       <Footer link={getAddExercise()} text='Добавить еще' />
     </main>
   ) : (
