0efa08c4580e651e921cba4e63cc07883f295a11
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2024-09-13 20:17:59 +0300

    Update function to add to firebase (weight))

--- src/entities/exercise/exercise-in-workout-info/ui/ExerciseInWorkoutInfo.tsx
+++ src/entities/exercise/exercise-in-workout-info/ui/ExerciseInWorkoutInfo.tsx
@@ -1,40 +1,69 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState } from 'react';
 import cl from "./ExerciseInWorkoutInfo.module.scss";
 import { Input } from 'shared/ui/input';
-import { useAppDispatch, useAppSelector } from 'shared/lib/hooks';
-import { addSetAndWeightInWorkout, IExerciseWorkout } from 'features/workout';
+import { addSetAndWeightInWorkout, IExerciseInfo } from 'features/workout';
+import { useAppDispatch } from 'shared/lib/hooks';
 
 interface IExerciseInWorkoutInfo {
 	exersiceID?: string;
 }
 
 export const ExerciseInWorkoutInfo: FC<IExerciseInWorkoutInfo> = ({ exersiceID }) => {
+	// Инициализация массива подходов с одним подходом, который имеет setID = 1
 
+	const dispatch = useAppDispatch()
+	const [sets, setSets] = useState<IExerciseInfo[]>([{ weight: null, reps: null, setID: 1 }]);
+
+
+	// Универсальная функция для обновления значения подхода
+	const updateSet = (index: number, key: 'weight' | 'reps', value: number | null) => {
+		const newSets = [...sets];
+		newSets[index] = { ...newSets[index], [key]: value };
+		setSets(newSets);
+		dispatch(addSetAndWeightInWorkout({ date: "2024-09-13", exerciseID: exersiceID, info: newSets[index] }))
+		console.log("newSets", newSets[index])
+
+	};
+
+	// console.log({ sets })
+	// Обработчик изменения значения (повторений или веса)
+	const handleChange = (index: number, key: 'weight' | 'reps') => (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value ? parseFloat(e.target.value) : null;
+		updateSet(index, key, value);
+	};
+
+	// Функция для добавления нового подхода
+	const addNewSet = () => {
+		const nextSetID = sets.length > 0 ? sets[sets.length - 1].setID + 1 : 1;
+		setSets([...sets, { weight: null, reps: null, setID: nextSetID }]);
+	};
 
 	return (
 		<div className={cl.workout_block}>
 			<ul className={cl.exercise_info}>
-				<li className={cl.set_info}>
-					<span className={cl.set_title}>1</span>
-					<Input
-						placeholder="Повторения"
-						type="number"
-						height='50px'
-					// value={info.reps ?? ''}
-					// onChange={(e) => handleRepsChange(index, e as React.ChangeEvent<HTMLInputElement>)}
-					// onBlur={updateWorkoutData}
-					/>
-					<Input
-						placeholder="Вес"
-						height='50px'
-						type="number"
-					// value={info.weight ?? ''}
-					// onChange={(e) => handleWeightChange(index, e as React.ChangeEvent<HTMLInputElement>)}
-					// onBlur={updateWorkoutData}
-					/>
-				</li>
+				{
+					sets.map((set, index) => (
+						<li className={cl.set_info} key={set.setID}>
+							<span className={cl.set_title}>{set.setID}</span>
+							<Input
+								placeholder="Повторения"
+								type="number"
+								height='50px'
+								value={set.reps ?? ''}
+								onChange={handleChange(index, 'reps')}
+							/>
+							<Input
+								placeholder="Вес"
+								height='50px'
+								type="number"
+								value={set.weight ?? ''}
+								onChange={handleChange(index, 'weight')}
+							/>
+						</li>
+					))
+				}
 			</ul>
-			{/* <span className={cl.addSet} onClick={addNewSet}>добавить подход</span> */}
+			<span className={cl.addSet} onClick={addNewSet}>добавить подход</span>
 		</div>
 	);
 };
